fc0a5c196be30aea29f7df6f655fbfd973b419d5
Michael Campos <mjcampos@users.noreply.github.com>
2025-04-22 17:10:24 +0200

    Two changes
    - Changed animation folder to input and moved input controllers in there
    - Set collision sound for ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,13 +1,17 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Ball : MonoBehaviour
 {
     float _launchForce = 600f;
     Rigidbody2D _rigidbody2D;
+    AudioSource _audiosSource;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _audiosSource = GetComponent<AudioSource>();
         
         LaunchBall();
     }
@@ -17,5 +21,10 @@ public class Ball : MonoBehaviour
         Vector2 direction = new Vector2(Random.Range(-1f, 1f), 1f).normalized;
         
         _rigidbody2D.AddForce(direction * _launchForce);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        _audiosSource.Play();
     }
 }
