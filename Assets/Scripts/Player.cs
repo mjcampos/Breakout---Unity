1449ba0b298e28e3d39d9c4e35b8eeeee764764c
Michael Campos <mjcampos@users.noreply.github.com>
2025-04-22 16:55:53 +0200

    Setup player input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,27 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour {
-    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float moveSpeed = 25f;
 
     Rigidbody2D _rigidbody2D;
+    float _moveInput;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
+    void OnMove(InputValue inputValue) {
+        _moveInput = inputValue.Get<Vector2>().x;
+    }
+
     void FixedUpdate()
     {
-        float input = Input.GetAxisRaw("Horizontal");
-        Vector2 direction = Vector2.right * input;
+        Vector2 direction = Vector2.right * _moveInput;
         Vector2 velocity = direction * moveSpeed;
-        Vector2 newPosition = velocity + _rigidbody2D.position;
         
-        _rigidbody2D.MovePosition(newPosition);
+        _rigidbody2D.linearVelocity = velocity;
     }
 }
