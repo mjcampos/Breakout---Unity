4b5f4a46b00365d8dcdd5624a35ac67c0a1fb0bc
Michael Campos <mjcampos@users.noreply.github.com>
2025-04-24 12:09:10 +0200

    Setup background music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour {
+    public static AudioManager Instance;
+    
+    [SerializeField] string[] scenesToStopMusic;
+    
+    
+    AudioSource _audioSource;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        foreach (string sceneName in scenesToStopMusic)
+        {
+            if (scene.name == sceneName)
+            {
+                _audioSource.Stop();
+                return;
+            }
+        }
+
+        if (!_audioSource.isPlaying)
+        {
+            _audioSource.Play();
+        }
+    }
+
+    void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+}
